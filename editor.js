1af54cf089c30547c20c738e437ed927f9e04a57
Avinasen <115348427+kitteneditorc0der@users.noreply.github.com>
2022-12-17 10:38:55 +0800

    Update editor.js

--- editor.js
+++ editor.js
@@ -9,3 +9,15 @@ document.body.onkeyup = function() {
   code.writeln("<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset='utf-8'><link href='" + csssrc.value + "' rel='stylesheet'><style>" + css.value + "</style></head><body>" + html.value + "</body><script src='" + jssrc.value +"'>" + js.value + "</script></html>");
   code.close();
 };
+var textAreas = document.getElementsByTagName("textarea");
+for(var i = 0; i < textAreas.length; i++) {
+	textAreas[i].addEventListener("keydown", function(event) {
+		if(event.key === 'Tab') {
+			event.preventDefault();
+			var start = this.selectionStart;
+			var end = this.selectionEnd;
+			this.value = this.value.substring(0, start) + "    " + this.value.substring(end);
+			this.selectionStart = this.selectionEnd = start + 4;
+		}
+	});
+}
