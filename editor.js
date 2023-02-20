72a278865ea17ee135da82089bad3660337b329b
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-02-20 09:12:15 +0800

    Update editor.js

--- editor.js
+++ editor.js
@@ -3,11 +3,9 @@ var css = document.getElementById("css");
 var csssrc = document.getElementById("csssrc");
 var js = document.getElementById("js");
 var jssrc = document.getElementById("jssrc");
-var code = document.getElementById("code").contentWindow.document;
+var code = document.getElementById("code");
 document.body.onkeyup = function() {
-  code.open();
-  code.writeln("<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset='utf-8'><link href='" + csssrc.value + "' rel='stylesheet'><style>" + css.value + "</style></head><body>" + html.value + "</body><script src='" + jssrc.value + "'></script><script>" + js.value + "<" + "/script></html>");
-  code.close();
+  code.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><meta charset='utf-8'><link href='" + csssrc.value + "' rel='stylesheet'><style>" + css.value + "</style></head><body>" + html.value + "</body><script src='" + jssrc.value + "'><" + "/script><script>" + js.value + "<" + "/script></html>");
 };
 var textAreas = document.getElementsByTagName("textarea");
 for(var i = 0; i < textAreas.length; i++) {
